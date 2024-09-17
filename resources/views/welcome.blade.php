70420b75f78ad47f51373a50a6506f25b2ad9535
“[mahsa <mahsaabarghani@gmail.com>
2024-09-17 23:17:31 +0330

    just commited

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@if (Route::has('login'))
+    <nav class="-mx-3 flex flex-1 justify-end">
+        @auth
+            <a href="{{ url('/dashboard') }}">
+                Dashboard
+            </a>
+        @else
+            <a href="{{ route('login') }}">
+                Log in
+            </a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">
+                    Register
+                </a>
+            @endif
+        @endauth
+    </nav>
+@endif
